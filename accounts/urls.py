2962845e6e712eae6249c4aace5a0c51f83097ef
Riyadulislamratul <riadul1810833319@gmail.com>
2026-01-23 01:11:08 +0600

    logout added

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='user_login'),
     path('dashboard/', views.dashboard, name='dashboard'),
-      path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('logout/', views.user_logout, name='user_logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -42,4 +42,6 @@ def user_login(request):
 def dashboard(request):
     return render(request, 'dashboard.html')
 
-
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
