a8c184bf7c2d3c85608fc5b958567e79b4871661
Riyadulislamratul <riadul1810833319@gmail.com>
2026-01-23 00:48:56 +0600

    register views added

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -6,4 +10,20 @@ def home(request):
     return render(request, 'index.html')
 
 def register(request):
+    if request.method == "POST":
+        User.objects.create_user(
+            username = request.POST.get("username"),
+            first_name = request.POST.get("first_name"),
+            last_name = request.POST.get("last_name"),
+            email = request.POST.get("email"),
+            phone = request.POST.get("phone"),
+            password = request.POST.get("password")
+
+            )
+        return redirect('user_login')
+        
+
     return render(request, 'register.html')
+    
+
+
